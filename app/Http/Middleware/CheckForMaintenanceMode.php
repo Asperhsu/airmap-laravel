96b1ddca4c5851dcfe17c358e8a4235cfada8ff8
Asper <asperwon@gmail.com>
2018-01-06 21:42:28 +0800

    implement maintenance white lists.

---
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Http\Exceptions\MaintenanceModeException;
+
+class CheckForMaintenanceMode
+{
+    /**
+     * The application implementation.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->app->isDownForMaintenance() &&
+            !in_array($request->getClientIp(), config('app.maintenance_white_lists'))) {
+            $data = json_decode(file_get_contents($this->app->storagePath().'/framework/down'), true);
+
+            throw new MaintenanceModeException($data['time'], $data['retry'], $data['message']);
+        }
+
+        return $next($request);
+    }
+}
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Service Unavailable</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 36px;
+                padding: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title">
+                    維護中，請稍後
+                </div>
+                <a href="https://www.facebook.com/g0vairmap/" target="_blank">
+                    <img src="https://z-m-static.xx.fbcdn.net/rsrc.php/v2/ya/r/O2aKM2iSbOw.png" alt="Facebook Page" class="img-responsive" height="50px">
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
