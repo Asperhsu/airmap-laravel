f14a89f2fe80371f6167b758c85dc74408972ea1
Asper <asperwon@gmail.com>
2018-01-26 20:26:13 +0800

    remove test code

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('icon/{value}', 'ApiController@makeIcon')->name('pm25icon');
 Route::get('widget/{group}${uuid}', 'ApiController@widget');
