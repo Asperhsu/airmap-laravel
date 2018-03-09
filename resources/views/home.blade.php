c35c00e935154f6a855292829eb07a6e0e340a89
Asper <asperwon@gmail.com>
2018-03-10 01:10:14 +0800

    modify redirect page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 
 @push('scripts')
 <script>
-    var seconds = 10;
+    var seconds = 20;
     var passSeconds = 0;
     setInterval(function () {
         passSeconds++;
         $("#countdown").text(seconds - passSeconds);
         if (passSeconds >= seconds) {
-            location.href = '{{ $redirectTo }}';
+            // location.href = '{{ $redirectTo }}';
         }
     }, 1000);
 </script>
@@ -29,14 +29,16 @@
 
 @section('content')
 <div class="center">
-    <h1>v4 版本將於 2018/12/31 停止服務</h1>
+    <div class="jumbotron" style="width: 60%">
+        <h1 class="display-4"><i class="fas fa-child"></i> 我們改版了</h1>
+        <p class="lead">v4 版本將於 2018/12/31 停止服務 <i class="fas fa-calendar-times"></i></p>
 
-    @if ($version === 'v5')
-    <h2><span id="countdown"></span> 秒後將跳轉到 v5 版本</h2>
-    @endif
+        <hr class="my-4">
 
-    @if ($version === 'v4')
-    <h2><span id="countdown"></span> 秒後將跳轉到 v4 版本</h2>
-    @endif
+        <p><i class="fas fa-stopwatch"></i> <span id="countdown"></span> 秒後將跳轉到 {{ $version }} 版本</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="{{ $redirectTo }}">或點選此處前往 <i class="fas fa-external-link-alt"></i></a>
+        </p>
+    </div>
 </div>
 @endsection
