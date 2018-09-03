08dc852c54fa7e998a433b07cf8432ef6addd4a0
Asper <asperwon@gmail.com>
2018-09-03 20:24:23 +0800

    change update interval

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('fetch:record')
                  ->timezone('Asia/Taipei')
                  ->everyMinute()
-                 ->withoutOverlapping();
+                 ->everyFiveMinutes();
 
         $schedule->command('fetch:lass-analysis')
                  ->timezone('Asia/Taipei')
