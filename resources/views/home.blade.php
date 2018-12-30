bf32ab75f010abfa87d8d8b04856d09c6e814700
Asper <asperwon@gmail.com>
2018-12-30 23:25:26 +0800

    edit home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,5 @@
 @extends('layouts.default')
 
-@push('scripts')
-<script>
-    var seconds = 20;
-    var passSeconds = 0;
-    setInterval(function () {
-        passSeconds++;
-        $("#countdown").text(seconds - passSeconds);
-        if (passSeconds >= seconds) {
-            location.href = '{{ $redirectTo }}';
-        }
-    }, 1000);
-</script>
-@endpush
-
 @section('style')
 <style>
     .center {
@@ -29,15 +15,17 @@
 
 @section('content')
 <div class="center">
-    <div class="jumbotron" style="width: 60%">
-        <h1 class="display-4"><i class="fas fa-child"></i> 我們改版了</h1>
-        <p class="lead">v4 版本將於 2018/12/31 停止服務 <i class="fas fa-calendar-times"></i></p>
-
+    <div class="jumbotron text-center" style="width: 60%">
+        <h1 class="display-4 text-danger">
+            <i class="fas fa-exclamation-circle"></i> <br/>
+            本網域 <small>(airmap.g0v.asper.tw)</small> <br/>將於 3/1 停用
+        </h1>
         <hr class="my-4">
 
-        <p><i class="fas fa-stopwatch"></i> <span id="countdown"></span> 秒後將跳轉到 {{ $version }} 版本</p>
         <p class="lead">
-            <a class="btn btn-primary btn-lg" href="{{ $redirectTo }}">或點選此處前往 <i class="fas fa-external-link-alt"></i></a>
+            <a class="btn btn-primary btn-lg" href="http://airmap.g0v.tw">
+                新網域為 http://airmap.g0v.tw
+            </a>
         </p>
     </div>
 </div>
